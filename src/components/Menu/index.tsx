fb3b4a85310597d7bff3e3ca29f0148631535bb0
Alisson Perim Honostorio <alisson.perim@base39.com.br>
2023-06-02 09:25:06 -0300

    logo change, some code changes

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -4,7 +4,7 @@ import { AppBarItemsEnum } from '../../enums'
 import { Image } from '@mui/icons-material'
 import MenuIcon from '@mui/icons-material/Menu';
 import { Img } from '../Img';
-import AppBarImage from '../../images/vilmar-fernandez-icon-appbar-icon.png'
+import AppBarImage from '../../images/vilmar-fernandez-icon-100-100-with-bgcolor.png'
 //onClick={handleDrawerToggle}
 
 function Menu() {
@@ -12,13 +12,13 @@ function Menu() {
         <>
             <Box sx={{ display: 'flex' }}>
                 <AppBar component={'div'}>
-                    <Toolbar>
+                    <Toolbar sx={{ backgroundColor: '#1E65F5' }}>
                         <Box sx={{ display: 'block', flexGrow: 1 }}>
-                            <Img src={AppBarImage} alt='image' />
+                            <Img src={AppBarImage} alt='image' sx={{ maxWidth: '40px', maxHeight: '40px' }} />
                         </Box>
                         <Box sx={{ display: { xl: 'block' } }}>
                             {AppBarItemsEnum.map((item) => (
-                                <Button key={item} sx={{ color: '#000000' }}>
+                                <Button key={item} sx={{ color: '#fff' }}>
                                     { item.toUpperCase() }
                                 </Button>
                             ))}
