655cb4b1672ee386fc2f4f6d857a95d281391a19
alissonperim <alisson.perim@base39.com.br>
2023-06-08 00:11:34 -0300

    remove ghost component

--- src/components/DrawerAppBar/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Box, Divider, Icon, List, ListItem, ListItemButton, ListItemText, MenuItem, Typography } from '@mui/material'
-import MenuIcon from '@mui/icons-material/Menu'
-import { AppBarItemsEnum } from '../../enums'
-
-export default function DrawerAppBar () {
-
-}
