7c3ab60120dcfbed5fd7c2dd1465ae3b95ca43c4
alissonperim <alisson.perim@base39.com.br>
2023-06-17 07:37:45 -0300

    upgrade the cards code

--- src/components/Card/styled.tsx
+++ src/components/Card/styled.tsx
@@ -3,9 +3,11 @@ import styled from 'styled-components'
 export const CardContainer = styled.div`
     display: flex;
     flex-direction: column;
-    background-color: rgba(230, 235, 224, 0.6);
+    background: #303030; background: -moz-linear-gradient(0deg,  #303030 0%,#5d5d5d 100%); background: -webkit-linear-gradient(0deg,  #303030 0%,#5d5d5d 100%); background: linear-gradient(0deg,  #303030 0%,#5d5d5d 100%);
     margin: 1rem;
     max-width: calc(100%/3);
+    position: relative;
+    min-height: 400px;
     width: 100%;
     flex: 1 0 25%;
     border-radius: 5px;
@@ -13,28 +15,45 @@ export const CardContainer = styled.div`
                 0px 4px 5px 0px rgba(0,0,0,0.1),
                 0px 1px 10px 0px rgba(0,0,0,0.1);
     &:hover {
-        transform: scale(1.02);
+        box-shadow: 0px 4px 5px -3px rgba(0,0,0,0.3),
+                    0px 6px 7px 0px rgba(0,0,0,0.3),
+                    0px 2px 12px 0px rgba(0,0,0,0.3);
         cursor: pointer;
     }
-    transition: ease-in-out 0.2s
+    transition: box-shadow ease-in-out 0.2s;
+    &:hover div h6 {
+        inset: auto auto 300px;
+        transition: inset .3s ease-out
+    };
+    &:hover div p {
+        opacity: 1;
+        transition: opacity .2s .4s ease-out
+    }
 `
 
-export const TextContainer = styled.span<{borderBottom?: boolean}>`
+export const TextContainer = styled.div<{borderBottom?: boolean}>`
     width: 100%;
     && > h6 {
-        font-size: 1.3rem;
-        padding-bottom: 0.5rem
+        font-size: 1.8rem;
+        padding-bottom: 0.5rem;
+        position: absolute;
+        inset: auto auto 30px;
+        transition: inset .3s .3s ease-out
     };
+
     & > p {
         font-size: 1.1rem;
+        inset: auto auto 80px;
+        opacity: 0;
+        position: absolute;
+        transition: opacity .3s ease-out
     };
     & > p, h6 {
         letter-spacing: .05rem;
         padding: 1.2rem;
         line-height: 1.4rem;
     };
-    color: rgba(21, 50, 67);
-    border-bottom: ${props => props.borderBottom ? '1px solid rgba(21, 50, 67)' : 'none'};
+    color: rgba(240, 246, 250);
 `
 
 export const CardsSection = styled.div`
